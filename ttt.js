a17804f8805c3d02364a681fe2606ca14925ff0e
Ahmed Alafoori <ahmedalafoori23@yahoo.com>
2024-07-19 21:58:43 +0300

    re-commit files

---
+++ ttt.js
@@ -0,0 +1,15 @@
+function greeting() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+  
+    if (hourNow < 12) {
+      greeting = "صباح الخير";
+    } else if (hourNow > 18) {
+      greeting = "مساء الخير";
+    } else {
+      greeting = "مرحبا";
+    }
+  
+    document.write(greeting);
+  }
